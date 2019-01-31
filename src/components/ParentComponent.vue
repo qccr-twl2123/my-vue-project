44881476406a128474587c16f03ed00be27fd77f
谢荣雳 <xrl6238@arcsoft.com.cn>
2019-01-31 16:30:01 +0800

    init

--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
+    传递数值给父组件:
     <h1>父组件</h1>
     <p>数值：{{total}}</p>
     <ChildComponent @increase="getTotal"></ChildComponent>
+
+    <br/>
+    <br/>
+    父组件监听子组件的事件:
     <ChildWithEvent :a='1' :b='2' v-on:add_event='getAddResult'></ChildWithEvent>
     计算结果:{{result}}
+    <br/>
+    <br/>
+    父组件传递参数给子组件:
+    <say-to pname='西游记'>龙马,孙悟空</say-to>
   </div>
 </template>
 
 <script>
 import ChildComponent from '@/components/ChildComponent';
 import ChildWithEvent from '@/components/ChildWithEvent';
+import SayTo from '@/components/SayTo';
 export default {
     name: "ParentComponent",
     data () {
@@ -21,7 +31,8 @@ export default {
     },
     components:{
       ChildComponent,
-      ChildWithEvent
+      ChildWithEvent,
+      SayTo
     },
     methods: {
       getTotal (count) {
---
+++ src/components/SayTo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    游戏名称:{{pname}}:<slot></slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "SayTo",
+        props: ['pname']
+    }
+</script>
+
+<style scoped>
+
+</style>
